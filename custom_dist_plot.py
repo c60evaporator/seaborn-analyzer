ca6067318f97b6f2a8e36320976f0d9bbc2b9c92
c60evaporator <c60evaporator@gmail.com>
2021-02-22 00:51:08 +0900

    桁数丸めメソッド追加＆ax未指定時も動作するよう修正

--- custom_dist_plot.py
+++ custom_dist_plot.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
+import decimal
 
 class dist():
     def _fit_norm(X: np.ndarray, sigmarange: float, linesplit: int):
@@ -34,23 +35,58 @@ class dist():
                 'pvalue':shapiro.pvalue,
             }
         return params, Xline, Yline
+    
+    def _round_digits(src: float, rounddigit: int = None, method='decimal'):
+        """
+        指定桁数で小数を丸める
 
-    def _round_dict_digits(srcdict: Dict[str, float], rounddigit: int = None):
+        Parameters
+        ----------
+        srcdict : Dict[str, float]
+            丸め対象のDict
+        rounddigit : int
+            フィッティング線の表示範囲（標準偏差の何倍まで表示するか指定）
+        method : int
+            桁数決定手法（'decimal':小数点以下, 'sig':有効数字(Decimal指定), 'format':formatで有効桁数指定）
+        """
+        if method == 'decimal':
+            return round(src, rounddigit)
+        elif method == 'sig':
+            with decimal.localcontext() as ctx:
+                ctx.prec = rounddigit
+                return ctx.create_decimal(src)
+        elif method == 'format':
+            return '{:.{width}g}'.format(src, width=rounddigit)
+
+    @classmethod
+    def _round_dict_digits(cls, srcdict: Dict[str, float], rounddigit: int = None, method='decimal'):
+        """
+        指定桁数でDictの値を丸める
+
+        Parameters
+        ----------
+        srcdict : Dict[str, float]
+            丸め対象のDict
+        rounddigit : int
+            フィッティング線の表示範囲（標準偏差の何倍まで表示するか指定）
+        method : int
+            桁数決定手法（'decimal':小数点以下, 'sig':有効数字(Decimal指定), 'format':formatで有効桁数指定）
+        """
         dstdict = {}
         for k, v in srcdict.items():
-            if isinstance(v, float):
-                dstdict[k] = round(v, rounddigit)
+            if rounddigit is not None and isinstance(v, float):
+                dstdict[k] = cls._round_digits(v, rounddigit=rounddigit, method=method)
             else:
                 dstdict[k] = v
         return dstdict
     
     # QQプロット
     @classmethod
-    def qqplot(cls, data: pd.Series, ax=None):
-        print('a')
+    def qqplot(cls, data: pd.Series, dist='norm', ax=None):
+        stats.probplot(data, dist=dist, plot=ax)
 
     @classmethod
-    def hist_dist(cls, data: pd.Series, ax=None, dist='norm', sigmarange=4, linecolor='red', linesplit=50, rounddigit=None):
+    def hist_dist(cls, data: pd.Series, dist='norm', ax=None, sigmarange=4, linecolor='red', linesplit=50, rounddigit=None):
         """
         分布フィッティングと各指標の表示
 
@@ -58,10 +94,10 @@ class dist():
         ----------
         data : pd.Series
             フィッティング対象のデータ
-        ax : 
-            表示対象のax（Noneなら新規作成）
         dist : str
             分布の種類 ("norm", "poisson", "exp", "chi2", "weibull", "lognorm", "gamma")
+        ax : matplotlib.axes._subplots.Axes
+            表示対象のax（Noneならplt.plotで1枚ごとにプロット）
         sigmarange : float
             フィッティング線の表示範囲 (標準偏差の何倍まで表示するか指定)
         linesplit : int
@@ -71,6 +107,10 @@ class dist():
         """
         # まずはヒストグラム描画
         sns.distplot(data, ax=ax, kde=False, norm_hist=True)
+        
+        # 描画用axがNoneのとき、matplotlib.pyplotを使用
+        if ax == None:
+            ax=plt
 
         # 分布をフィッティング
         X = data.values
@@ -82,7 +122,7 @@ class dist():
         ax.plot(Xline, Yline, color=linecolor)
 
         # パラメータを記載
-        param_list = [f'{k}={v}' for k, v in cls._round_dict_digits(params, rounddigit).items()]
+        param_list = [f'{k}={v}' for k, v in cls._round_dict_digits(params, rounddigit, 'sig').items()]
         param_text = "\n".join(param_list)
         ax.text(np.amax(Xline), np.amax(Yline), param_text, verticalalignment='top', horizontalalignment='right')
 
