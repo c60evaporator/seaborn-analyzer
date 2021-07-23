2ce2875e98d419f632d7108ea1241922b7b36a0d
c60evaporator <c60evaporator@gmail.com>
2021-07-23 21:11:36 +0900

    setup.py作成

---
+++ setup.py
@@ -0,0 +1,80 @@
+# Author: Kenta Nakamura <c60evaporator@gmail.com>
+# Copyright (c) 2020-2021 Kenta Nakamura
+# License: BSD 3 clause
+
+from setuptools import setup
+
+DESCRIPTION = "seaborn_analyzer: data visualization of regression, classification and distribution"
+NAME = 'seaborn_analyzer'
+AUTHOR = 'Kenta Nakamura'
+AUTHOR_EMAIL = 'c60evaporator@gmail.com'
+URL = 'https://github.com/c60evaporator/seaborn_analyzer'
+LICENSE = 'BSD 3-Clause'
+DOWNLOAD_URL = 'https://github.com/c60evaporator/seaborn_analyzer'
+VERSION = '0.1.1'
+PYTHON_REQUIRES = ">=3.6"
+
+INSTALL_REQUIRES = [
+    'matplotlib>=3.3.4',
+    'seaborn>=0.11.1',
+    'numpy >=1.20.3',
+    'pandas>=1.2.4',
+    'matplotlib>=3.3.4',
+    'scipy>=1.6.3',
+    'scikit-learn>=0.24.2',
+]
+
+EXTRAS_REQUIRE = {
+    'tutorial': [
+        'mlxtend>=0.18.0',
+        'xgboost>=1.4.2',
+    ]
+}
+
+PACKAGES = [
+    'seaborn_analyzer'
+]
+
+CLASSIFIERS = [
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Multimedia :: Graphics',
+    'Framework :: Matplotlib :: Seaborn',
+]
+
+LONG_DESCRIPTION = """
+A data visualization tool of regression, classification and distribution.
+Contact
+=============
+If you have any questions or comments about seaborn_analyzer,
+please feel free to contact me via
+eMail: c60evaporator@gmail.com
+or Twitter: https://twitter.com/c60evaporator
+This project is hosted at https://github.com/c60evaporator/seaborn_analyzer
+"""
+
+setup(name=NAME,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      maintainer=AUTHOR,
+      maintainer_email=AUTHOR_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE,
+      packages=PACKAGES,
+      classifiers=CLASSIFIERS
+    )
--- tutorial.py
+++ tutorial.py
@@ -1,260 +1,259 @@
-# #######散布図行列と相関係数行列 custom_pair_plot.py (https://qiita.com/c60evaporator/items/20f11b6ee965cec48570)######
-# #%% custom_pair_plot
-# from seaborn_analyzer import CustomPairPlot
-# import seaborn as sns
+#######散布図行列と相関係数行列 custom_pair_plot.py (https://qiita.com/c60evaporator/items/20f11b6ee965cec48570)######
+#%% custom_pair_plot
+from seaborn_analyzer import CustomPairPlot
+import seaborn as sns
 
-# titanic = sns.load_dataset("titanic")
-# cp = CustomPairPlot()
-# cp.pairanalyzer(titanic, hue='survived')
-
-
-
-# #######ヒストグラムと各種分布フィッティング custom_hist_plot.py (https://qiita.com/c60evaporator/items/fc531aff0cdbafac0f42)######
-# #%% ヒストグラム表示
-# import seaborn as sns
-# iris = sns.load_dataset("iris")
-# sns.distplot(iris['sepal_width'], kde=False)
-# # %% 概要の「機能1」（正規性検定とQQプロット）
-# import seaborn as sns
-# from seaborn_analyzer import hist
-# iris = sns.load_dataset("iris")
-# hist.plot_normality(iris['sepal_width'])
-# #%% 概要の「機能2」（分布フィッティングと評価指標算出）
-# from seaborn_analyzer import hist
-# from sklearn.datasets import load_boston
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# from scipy import stats
-# df = pd.DataFrame(load_boston().data, columns= load_boston().feature_names)
-# all_params, all_scores = hist.fit_dist(df, x='LSTAT', dist=['norm', 'gamma', 'lognorm', 'uniform'])
-# df_scores = pd.DataFrame(all_scores).T
-# df_scores
-# # %% Bostonデータセットでの正規性検定とQQプロット
-# from seaborn_analyzer import hist
-# from sklearn.datasets import load_boston
-# import pandas as pd
-# df = pd.DataFrame(load_boston().data, columns= load_boston().feature_names)
-# hist.plot_normality(df, x='LSTAT', norm_hist=False, rounddigit=5)
-# #%% ガンマ分布をパラメータ変えてフィッティング
-# from seaborn_analyzer import hist
-# from sklearn.datasets import load_boston
-# from scipy import stats
-# import pandas as pd
-# import numpy as np
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# df = pd.DataFrame(load_boston().data, columns= load_boston().feature_names)
-# X = df['LSTAT'].values
-# sns.distplot(X, kde=False, norm_hist=True, hist_kws={'alpha':0.7, 'edgecolor':'white'})
-# Xline = np.linspace(min(np.mean(X) - np.std(X) * 4, np.amin(X)), max(np.mean(X) + np.std(X) * 4, np.amax(X)), 100)
-# line_legends = []
-# line_labels = []
-# line_colors = ['red', 'darkmagenta', 'mediumblue', 'darkorange']
-# params = [(1.0, 3.2),
-#           (4.0, 3.2),
-#           (4.0, 6.4),
-#           (8.0, 3.2)]
-# for i, (k, theta) in enumerate(params):
-#     # 対数尤度
-#     logL = np.sum(stats.gamma.logpdf(X, loc=0, scale=k, a=theta))
-#     # 線の描画
-#     Yline = stats.gamma.pdf(Xline, loc=0, scale=k, a=theta)
-#     leg, = plt.plot(Xline, Yline, color=line_colors[i])
-#     line_legends.append(leg)
-#     line_labels.append(f'k={k}, θ={theta}, logL={round(logL, 1)}')
-# plt.legend(line_legends, line_labels, loc='upper right')
-
-# # %% 正規分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# mean_list = [0, 1]
-# std_list = [1, 2]
-# loc = 0
-# x = np.linspace(-8, 8, 200)
-# for mean in mean_list:
-#     for std in std_list:
-#         plt.plot(x, stats.norm.pdf(x, scale=std, loc=mean), label=f'mean={mean}, std={std}')
-#         plt.legend()
-# # %% 対数正規分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# mu_list = [0, 1]
-# sigma_list = [1, 2]
-# loc = 0
-# x = np.linspace(0, 4, 200)
-# for mu in mu_list:
-#     for sigma in sigma_list:
-#         plt.plot(x, stats.lognorm.pdf(x, s=sigma, scale=np.exp(mu), loc=loc), label=f'mu={mu}, sigma={sigma}')
-#         plt.legend()
-# # %% ガンマ分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# theta_list = [0.5, 1]
-# k_list = [1, 3]
-# loc = 0
-# x = np.linspace(0, 8, 200)
-# for theta in theta_list:
-#     for k in k_list:
-#         plt.plot(x, stats.gamma.pdf(x, a=k, scale=theta, loc=loc), label=f'theta={theta}, k={k}')
-#         plt.legend()
-# # %% コーシー分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# scale = 1
-# loc = 0
-# x = np.linspace(-4, 4, 200)
-# plt.plot(x, stats.cauchy.pdf(x, scale=scale, loc=loc))
-# # %% t分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# df_list = [1, 2, 3, 4]
-# scale = 1
-# loc = 0
-# x = np.linspace(-4, 4, 200)
-# for df in df_list:
-#     plt.plot(x, stats.t.pdf(x, df=df, scale=scale, loc=loc), label=f'df={df}')
-#     plt.legend()
-# # %% パレート分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# b_list = [1, 2, 3, 4]
-# scale = 1
-# loc = 0
-# x = np.linspace(0, 4, 200)
-# for b in b_list:
-#     plt.plot(x, stats.pareto.pdf(x, b=b, scale=scale, loc=loc), label=f'b={b}')
-#     plt.legend()
-# # %% 一様分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# scale = 1
-# loc = 0
-# x = np.linspace(-0.2, 1.2, 200)
-# plt.plot(x, stats.uniform.pdf(x, scale=scale, loc=loc))
-# # %% 指数分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# lambda_list = [0.5, 1, 2]
-# loc = 0
-# x = np.linspace(0, 4, 200)
-# for lm in lambda_list:
-#     plt.plot(x, stats.expon.pdf(x, scale=1/lm, loc=loc), label=f'lambda={lm}')
-#     plt.legend()
-# # %% ワイブル分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# c_list = [0.5, 1, 2, 4]
-# scale = 1
-# loc = 0
-# x = np.linspace(0, 4, 200)
-# for c in c_list:
-#     plt.plot(x, stats.weibull_min.pdf(x, c=c, scale=scale, loc=loc), label=f'c={c}')
-#     plt.legend()
-# # %% カイ二乗分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# df_list = [1, 2, 3, 4]
-# scale = 1
-# loc = 0
-# x = np.linspace(0.2, 8, 200)
-# for df in df_list:
-#     plt.plot(x, stats.chi2.pdf(x, df=df, scale=scale, loc=loc), label=f'df={df}')
-#     plt.legend()
-# # %% 二項分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# n = 100
-# p_list = [0.01, 0.05, 0.1, 0.2]
-# loc = 0
-# x = np.linspace(0, 20, 21)
-# for p in p_list:
-#     plt.plot(x, stats.binom.pmf(x, n=n, p=p, loc=loc), label=f'p={p}')
-#     plt.legend()
-# # %% ポアソン分布
-# from scipy import stats
-# import numpy as np
-# import matplotlib.pyplot as plt
-# mu_list = [1, 5, 10, 20]
-# loc = 0
-# x = np.linspace(0, 20, 21)
-# for mu in mu_list:
-#     plt.plot(x, stats.poisson.pmf(x, mu=mu, loc=loc), label=f'mu={mu}')
-#     plt.legend()
+titanic = sns.load_dataset("titanic")
+cp = CustomPairPlot()
+cp.pairanalyzer(titanic, hue='survived')
 
 
+#######ヒストグラムと各種分布フィッティング custom_hist_plot.py (https://qiita.com/c60evaporator/items/fc531aff0cdbafac0f42)######
+#%% ヒストグラム表示
+import seaborn as sns
+iris = sns.load_dataset("iris")
+sns.distplot(iris['sepal_width'], kde=False)
+# %% 概要の「機能1」（正規性検定とQQプロット）
+import seaborn as sns
+from seaborn_analyzer import hist
+iris = sns.load_dataset("iris")
+hist.plot_normality(iris['sepal_width'])
+#%% 概要の「機能2」（分布フィッティングと評価指標算出）
+from seaborn_analyzer import hist
+from sklearn.datasets import load_boston
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+df = pd.DataFrame(load_boston().data, columns= load_boston().feature_names)
+all_params, all_scores = hist.fit_dist(df, x='LSTAT', dist=['norm', 'gamma', 'lognorm', 'uniform'])
+df_scores = pd.DataFrame(all_scores).T
+df_scores
+# %% Bostonデータセットでの正規性検定とQQプロット
+from seaborn_analyzer import hist
+from sklearn.datasets import load_boston
+import pandas as pd
+df = pd.DataFrame(load_boston().data, columns= load_boston().feature_names)
+hist.plot_normality(df, x='LSTAT', norm_hist=False, rounddigit=5)
+#%% ガンマ分布をパラメータ変えてフィッティング
+from seaborn_analyzer import hist
+from sklearn.datasets import load_boston
+from scipy import stats
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+df = pd.DataFrame(load_boston().data, columns= load_boston().feature_names)
+X = df['LSTAT'].values
+sns.distplot(X, kde=False, norm_hist=True, hist_kws={'alpha':0.7, 'edgecolor':'white'})
+Xline = np.linspace(min(np.mean(X) - np.std(X) * 4, np.amin(X)), max(np.mean(X) + np.std(X) * 4, np.amax(X)), 100)
+line_legends = []
+line_labels = []
+line_colors = ['red', 'darkmagenta', 'mediumblue', 'darkorange']
+params = [(1.0, 3.2),
+          (4.0, 3.2),
+          (4.0, 6.4),
+          (8.0, 3.2)]
+for i, (k, theta) in enumerate(params):
+    # 対数尤度
+    logL = np.sum(stats.gamma.logpdf(X, loc=0, scale=k, a=theta))
+    # 線の描画
+    Yline = stats.gamma.pdf(Xline, loc=0, scale=k, a=theta)
+    leg, = plt.plot(Xline, Yline, color=line_colors[i])
+    line_legends.append(leg)
+    line_labels.append(f'k={k}, θ={theta}, logL={round(logL, 1)}')
+plt.legend(line_legends, line_labels, loc='upper right')
 
-# #######分類モデルの可視化 custom_scatter_plot.classplot ()######
-# # %% 概要の「機能1」（決定境界）
-# import seaborn as sns
-# from sklearn.svm import SVC
-# from seaborn_analyzer import classplot
-# iris = sns.load_dataset("iris")
-# model = SVC()
-# classplot.class_separator_plot(model, ['petal_width', 'petal_length'], 'species', iris)
-# # %% 概要の「機能2」（クラス確率）
-# import seaborn as sns
-# from sklearn.svm import SVC
-# from seaborn_analyzer import classplot
-# iris = sns.load_dataset("iris")
-# model = SVC(probability=True)
-# classplot.class_proba_plot(model, ['petal_width', 'petal_length'], 'species', iris,
-#                            proba_type='contourf')
-# # %% アヤメ散布図
-# import seaborn as sns
-# iris = sns.load_dataset("iris")
-# sns.scatterplot(x='petal_width', y='petal_length', data=iris,
-#                 hue='species', palette=['green', 'red', 'blue'])
-# # %% サポートベクターマシン分類して性能評価指標算出
-# from sklearn.svm import SVC
-# from sklearn.metrics import accuracy_score, f1_score
-# X = iris[['petal_width', 'petal_length']].values  # 説明変数
-# y = iris['species'].values  # 目的変数(3種類のアヤメ種類)
-# model = SVC()  # SVM分類用インスタンス
-# model.fit(X, y)  # 学習
-# y_pred = model.predict(X)  # 推論
-# print(f'Accuracy={accuracy_score(y, y_pred)}')  # 正解率を表示
-# print(f'F1={f1_score(y, y_pred, average="macro")}')  # F1-Macroを表示
-# # %% mlxtendで決定境界可視化
-# from mlxtend.plotting import plot_decision_regions
-# import numpy as np
-# # 目的変数の配列を2次元に変換
-# y = y.reshape(len(y), 1)
-# # 目的変数をstr型→int型に変換
-# label_names = list(dict.fromkeys(y[:, 0]))
-# label_dict = dict(zip(label_names, range(len(label_names))))
-# y_int=np.vectorize(lambda x: label_dict[x])(y)
-# # 学習
-# model.fit(X, y_int)
-# # mlxtendで決定境界可視化
-# plot_decision_regions(X, y_int[:, 0], clf=model,
-#                       colors='green,red,blue')
-# # %% 本ツールによる決定境界可視化
-# from seaborn_analyzer import classplot
-# model = SVC()
-# classplot.class_separator_plot(model, ['petal_width', 'petal_length'], 'species', iris)
-# # %% ランダムフォレスト回帰での描画例
-# from sklearn.ensemble import RandomForestClassifier
-# model = RandomForestClassifier()
-# classplot.class_separator_plot(model, ['petal_width', 'petal_length'], 'species', iris)
-# # %% 2分割クロスバリデーション
-# model = SVC()
-# classplot.class_separator_plot(model, ['petal_width', 'petal_length'], 'species', iris,
-#                                cv=2, display_cv_indices = [0, 1])
-# # %% 3次元特徴量
-# classplot.class_separator_plot(model, ['petal_width', 'petal_length', 'sepal_width'], 'species', iris,
-#                                x_chart=['petal_width', 'petal_length'],
-#                                pair_sigmarange = 1.0, pair_sigmainterval = 0.5)
+# %% 正規分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+mean_list = [0, 1]
+std_list = [1, 2]
+loc = 0
+x = np.linspace(-8, 8, 200)
+for mean in mean_list:
+    for std in std_list:
+        plt.plot(x, stats.norm.pdf(x, scale=std, loc=mean), label=f'mean={mean}, std={std}')
+        plt.legend()
+# %% 対数正規分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+mu_list = [0, 1]
+sigma_list = [1, 2]
+loc = 0
+x = np.linspace(0, 4, 200)
+for mu in mu_list:
+    for sigma in sigma_list:
+        plt.plot(x, stats.lognorm.pdf(x, s=sigma, scale=np.exp(mu), loc=loc), label=f'mu={mu}, sigma={sigma}')
+        plt.legend()
+# %% ガンマ分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+theta_list = [0.5, 1]
+k_list = [1, 3]
+loc = 0
+x = np.linspace(0, 8, 200)
+for theta in theta_list:
+    for k in k_list:
+        plt.plot(x, stats.gamma.pdf(x, a=k, scale=theta, loc=loc), label=f'theta={theta}, k={k}')
+        plt.legend()
+# %% コーシー分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+scale = 1
+loc = 0
+x = np.linspace(-4, 4, 200)
+plt.plot(x, stats.cauchy.pdf(x, scale=scale, loc=loc))
+# %% t分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+df_list = [1, 2, 3, 4]
+scale = 1
+loc = 0
+x = np.linspace(-4, 4, 200)
+for df in df_list:
+    plt.plot(x, stats.t.pdf(x, df=df, scale=scale, loc=loc), label=f'df={df}')
+    plt.legend()
+# %% パレート分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+b_list = [1, 2, 3, 4]
+scale = 1
+loc = 0
+x = np.linspace(0, 4, 200)
+for b in b_list:
+    plt.plot(x, stats.pareto.pdf(x, b=b, scale=scale, loc=loc), label=f'b={b}')
+    plt.legend()
+# %% 一様分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+scale = 1
+loc = 0
+x = np.linspace(-0.2, 1.2, 200)
+plt.plot(x, stats.uniform.pdf(x, scale=scale, loc=loc))
+# %% 指数分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+lambda_list = [0.5, 1, 2]
+loc = 0
+x = np.linspace(0, 4, 200)
+for lm in lambda_list:
+    plt.plot(x, stats.expon.pdf(x, scale=1/lm, loc=loc), label=f'lambda={lm}')
+    plt.legend()
+# %% ワイブル分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+c_list = [0.5, 1, 2, 4]
+scale = 1
+loc = 0
+x = np.linspace(0, 4, 200)
+for c in c_list:
+    plt.plot(x, stats.weibull_min.pdf(x, c=c, scale=scale, loc=loc), label=f'c={c}')
+    plt.legend()
+# %% カイ二乗分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+df_list = [1, 2, 3, 4]
+scale = 1
+loc = 0
+x = np.linspace(0.2, 8, 200)
+for df in df_list:
+    plt.plot(x, stats.chi2.pdf(x, df=df, scale=scale, loc=loc), label=f'df={df}')
+    plt.legend()
+# %% 二項分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+n = 100
+p_list = [0.01, 0.05, 0.1, 0.2]
+loc = 0
+x = np.linspace(0, 20, 21)
+for p in p_list:
+    plt.plot(x, stats.binom.pmf(x, n=n, p=p, loc=loc), label=f'p={p}')
+    plt.legend()
+# %% ポアソン分布
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+mu_list = [1, 5, 10, 20]
+loc = 0
+x = np.linspace(0, 20, 21)
+for mu in mu_list:
+    plt.plot(x, stats.poisson.pmf(x, mu=mu, loc=loc), label=f'mu={mu}')
+    plt.legend()
+
+
+
+#######分類モデルの可視化 custom_scatter_plot.classplot ()######
+# %% 概要の「機能1」（決定境界）
+import seaborn as sns
+from sklearn.svm import SVC
+from seaborn_analyzer import classplot
+iris = sns.load_dataset("iris")
+model = SVC()
+classplot.class_separator_plot(model, ['petal_width', 'petal_length'], 'species', iris)
+# %% 概要の「機能2」（クラス確率）
+import seaborn as sns
+from sklearn.svm import SVC
+from seaborn_analyzer import classplot
+iris = sns.load_dataset("iris")
+model = SVC(probability=True)
+classplot.class_proba_plot(model, ['petal_width', 'petal_length'], 'species', iris,
+                           proba_type='contourf')
+# %% アヤメ散布図
+import seaborn as sns
+iris = sns.load_dataset("iris")
+sns.scatterplot(x='petal_width', y='petal_length', data=iris,
+                hue='species', palette=['green', 'red', 'blue'])
+# %% サポートベクターマシン分類して性能評価指標算出
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, f1_score
+X = iris[['petal_width', 'petal_length']].values  # 説明変数
+y = iris['species'].values  # 目的変数(3種類のアヤメ種類)
+model = SVC()  # SVM分類用インスタンス
+model.fit(X, y)  # 学習
+y_pred = model.predict(X)  # 推論
+print(f'Accuracy={accuracy_score(y, y_pred)}')  # 正解率を表示
+print(f'F1={f1_score(y, y_pred, average="macro")}')  # F1-Macroを表示
+# %% mlxtendで決定境界可視化
+from mlxtend.plotting import plot_decision_regions
+import numpy as np
+# 目的変数の配列を2次元に変換
+y = y.reshape(len(y), 1)
+# 目的変数をstr型→int型に変換
+label_names = list(dict.fromkeys(y[:, 0]))
+label_dict = dict(zip(label_names, range(len(label_names))))
+y_int=np.vectorize(lambda x: label_dict[x])(y)
+# 学習
+model.fit(X, y_int)
+# mlxtendで決定境界可視化
+plot_decision_regions(X, y_int[:, 0], clf=model,
+                      colors='green,red,blue')
+# %% 本ツールによる決定境界可視化
+from seaborn_analyzer import classplot
+model = SVC()
+classplot.class_separator_plot(model, ['petal_width', 'petal_length'], 'species', iris)
+# %% ランダムフォレスト回帰での描画例
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier()
+classplot.class_separator_plot(model, ['petal_width', 'petal_length'], 'species', iris)
+# %% 2分割クロスバリデーション
+model = SVC()
+classplot.class_separator_plot(model, ['petal_width', 'petal_length'], 'species', iris,
+                               cv=2, display_cv_indices = [0, 1])
+# %% 3次元特徴量
+classplot.class_separator_plot(model, ['petal_width', 'petal_length', 'sepal_width'], 'species', iris,
+                               x_chart=['petal_width', 'petal_length'],
+                               pair_sigmarange = 1.0, pair_sigmainterval = 0.5)
 # %% 4次元特徴量
 classplot.class_separator_plot(model, ['petal_width', 'petal_length', 'sepal_width', 'sepal_length'], 'species', iris,
                                x_chart=['petal_width', 'petal_length'],
