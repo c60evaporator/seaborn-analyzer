836652095264bb059e3d91907a0b2b50a0bed1bd
knakamura <c60evaporator@gmail.com>
2023-06-20 23:33:57 -0700

    Modify test code

--- tests/test_cv.py
+++ tests/test_cv.py
@@ -6,7 +6,11 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import KFold
 import lightgbm as lgb
 
-from seaborn_analyzer import cross_val_score_eval_set
+import os
+import sys
+ROOT = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(ROOT)
+from seaborn_analyzer._cv_eval_set import cross_val_score_eval_set
 
 class TestCvClassifiers:
     # Test data
